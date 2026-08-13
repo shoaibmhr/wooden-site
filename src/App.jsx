356adb6db46adddcd2cad0c63f09ecf3e699dc27
shoaibmhr <shoaibmhr895@gmail.com>
2026-08-13 18:16:20 +0500

    message

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,9 @@
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import About from './components/About'
-import FeaturedProjects from './components/FeaturedProjects'
-import Services from './components/Services'
-import WhyChooseUs from './components/WhyChooseUs'
-import Process from './components/Process'
-import Testimonials from './components/Testimonials'
-import CTA from './components/CTA'
-import Footer from './components/Footer'
-
 function App() {
   return (
-    <>
-      <Navbar />
-
-      <main>
-        <Hero />
-        <About />
-        <FeaturedProjects />
-        <Services />
-        <WhyChooseUs />
-        <Process />
-        <Showcase />
-        <Testimonials />
-        <CTA />
-      </main>
-
-      <Footer />
-    </>
-  )
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Tailwind CSS Working!</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
